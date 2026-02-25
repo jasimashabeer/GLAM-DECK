ab7e9da1f7ad1759cc6ae3214eb016485b5ecff7
jasimashabeer <jasimashabeer@gmail.com>
2026-02-25 11:10:21 +0530

    Fixed Google OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const env = require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://glamdeck.shop/auth/google/callback'
 },
     async (accessToken, refreshToken, Profile, done) => {
         try {
